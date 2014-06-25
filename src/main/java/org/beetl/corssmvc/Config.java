f68a557c371eabb981effd84e8679a75ee58c3a8
javamonkey@163.com <javamonkey@163.com>
2014-06-25 18:23:06 +0800

    refactory

---
+++ src/main/java/org/beetl/corssmvc/Config.java
@@ -0,0 +1,31 @@
+package org.beetl.corssmvc;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Config {
+	Properties ps = new Properties();
+	public Config(String path) throws IOException{
+		ps.load(new FileInputStream(new File(path)));
+	}
+	
+	public String get(String key){
+		return ps.getProperty(key);
+	}
+	
+	public int getInt(String key,int value){
+		String str = ps.getProperty(key);
+		if(str==null)return value;
+		else return Integer.parseInt(str);
+		
+	}
+	
+	public String get(String key,String defaulPath){
+		String str = ps.getProperty(key);
+		if(str==null)return defaulPath;
+		else return str;
+	}
+	
+}
